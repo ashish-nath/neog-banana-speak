871040d397597b60391f7e26048ff838272ecc0f
ashish-nath <84774484+ashish-nath@users.noreply.github.com>
2021-06-06 13:04:25 +0530

    Create app.js

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
-<<<<<<< HEAD
-var btnTranslate=document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
-var outputDiv=document.querySelector("#output");
-=======
-//input
-var username = prompt("Give me username")
->>>>>>> parent of 0ff0525 (updated file)
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-//processing
-var welcomeMessage = "this script works!" + username;
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-<<<<<<< HEAD
-var serverURL="https://api.funtranslations.com/translate/minion.json"
- 
-function getTranslationURL(input){
-     return serverURL + "?" + "text=" + input
- }
-
- function errorHandler(error){
-     console.log("error occured", error);
-     alert("something wrong with the server! try again after sometime")
- }
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
- function clickmyHandler(){
-     var inputText=txtInput.value; //taking input
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
 
-     //calling server for processing
-     fetch(getTranslationURL(inputText))
-         .then(response => response.json())
-         .then(json => {
-             var translatedText = json.contents.translated;
-             outputDiv.innerText=translatedText; //output
-         })
-         .catch(errorHandler)
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click",clickmyHandler)
-=======
-//output
-alert(welcomeMessage);
->>>>>>> parent of 0ff0525 (updated file)
+btnTranslate.addEventListener("click", clickHandler)
